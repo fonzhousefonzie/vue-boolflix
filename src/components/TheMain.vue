ffa9c57cf4a5088eac27e2f24664f7198d884bdf
fonzhousefonzie <harth.fonz@gmail.com>
2022-06-28 17:59:43 +0200

    milestone 1

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -2,11 +2,11 @@
     <div>
         <h4>Movies</h4>
         <ul>
-            <li v-for="movie in findMovies" :key="movie.title">{{movie.title}}</li>
+            <li v-for="movie in findMovies" :key="movie.title"><strong>Titolo:</strong> {{movie.title}}<br><strong>Titolo originale:</strong> {{movie.original_title}}<br><strong>Lingua Originale:</strong> {{movie.original_language}}<br><strong>Voto:</strong> {{movie.vote_average}}</li>
         </ul>
         <h4>Series</h4>
         <ul>
-            <li v-for="serie in findSeries" :key="serie.title">{{serie.title}}</li>
+            <li v-for="serie in findSeries" :key="serie.name"><strong>Titolo:</strong> {{serie.name}}<br><strong>Titolo originale:</strong> {{serie.original_name}}<br><strong>Lingua Originale:</strong> {{serie.original_language}}<br><strong>Voto:</strong> {{serie.vote_average}}</li>
         </ul>
     </div>
 </template>
